f3a89fcf94629995bd5dbe53deda4fbdca0360a1
QTR2026 <wrs65641@gmail.com>
2026-02-03 05:28:26 +0300

    Create megaDB.js

---
+++ megaDB.js
@@ -0,0 +1,8 @@
+const megaDB = [
+  { n: "The Godfather", t: "فيلم", img: "https://...", link: "https://www.imdb.com/title/tt0068646/" },
+  { n: "Naruto", t: "أنمي", img: "https://...", link: "https://www.imdb.com/title/tt0409591/" },
+  { n: "صدام حسين", t: "شخصية", img: "https://...", link: "https://ar.wikipedia.org/wiki/صدام_حسين" },
+  { n: "Toyota Supra", t: "سيارة", img: "https://...", link: "https://ar.wikipedia.org/wiki/تويوتا_سوبرا" },
+  // ... باقي الـ 2350 عنصر
+];
+<script src="megaDB.js"></script>
